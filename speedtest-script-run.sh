d8ab774c061c1acb585888923b5d297460b32b1a
saad <saad.aslam39@gmail.com>
2020-07-15 18:15:04 -0500

    - closing parantheses on last updated - adding a render script at the end so the dashboard updates ... maybe add a filter later and make it a dynamic shiny app of some sort

--- speedtest-script-run.sh
+++ speedtest-script-run.sh
@@ -3,4 +3,5 @@
 cd ~/r_stuff/speedtest-report/
 
 /Users/saad/r_stuff/speedtest-report/speedtest-script.R
-
+export RSTUDIO_PANDOC='/Applications/RStudio.app/Contents/MacOS/pandoc'
+Rscript -e 'rmarkdown::render(here::here("dashboard.Rmd"))'
