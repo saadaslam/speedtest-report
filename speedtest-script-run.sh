727a230f2c87846bd05ad988126ae65ac9005cb5
saad <saad.aslam39@gmail.com>
2020-07-15 19:05:52 -0500

    using full path for Rscript

--- speedtest-script-run.sh
+++ speedtest-script-run.sh
@@ -4,4 +4,5 @@ cd ~/r_stuff/speedtest-report/
 
 /Users/saad/r_stuff/speedtest-report/speedtest-script.R
 export RSTUDIO_PANDOC='/Applications/RStudio.app/Contents/MacOS/pandoc'
-Rscript -e 'rmarkdown::render(here::here("dashboard.Rmd"))'
+/usr/local/bin/Rscript -e 'rmarkdown::render(here::here("dashboard.Rmd"))'
+
